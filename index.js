bafd63cdddab722394c433e5c4d90279334583bf
Burhanuddin-49 <burhanuddin9651@gmail.com>
2024-01-01 07:13:55 +0530

    added ejs file for server side rendering

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { connectToMongoose } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+const path = require("path");
+const staticRouter = require("./routes/staticRouter");
 
 const app = express();
 const PORT = 8001;
@@ -10,11 +12,17 @@ connectToMongoose(
   "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1/short-url"
 ).then(() => console.log("Mongodb connected"));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", staticRouter);
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
