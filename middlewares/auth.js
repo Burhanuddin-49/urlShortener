a2c2590a91183412c5f1c184eed1d1b3be5b9039
Burhanuddin-49 <burhanuddin9651@gmail.com>
2024-01-08 01:38:26 +0530

    added authorization method for NORMAL and ADMIN

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,25 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.headers["authorization"];
+const checkForAuthentication = (req, res, next) => {
+  const tokenCookie = req.cookies?.token;
+  req.user = null;
+  if (!tokenCookie) return next();
 
-  if (!userUid) return res.redirect("/login");
-  const token = userUid.split("Bearer ")[1];
+  const token = tokenCookie;
   const user = getUser(token);
-
-  if (!user) return res.redirect("/login");
-
   req.user = user;
-  next();
-}
+  return next();
+};
 
-async function checkAuth(req, res, next) {
-  const userUid = req.headers["authorization"];
-  const token = userUid.split("Bearer ")[1];
-  const user = getUser(token);
-
-  req.user = user;
-  next();
+function restrictTo(role = []) {
+  return function (req, res, next) {
+    if (!req.user) return res.redirect("/login");
+    if (!role.includes(req.user.role)) return res.end("unAuthorized");
+    return next();
+  };
 }
 
 module.exports = {
-  restrictToLoggedinUserOnly,
-  checkAuth,
+  checkForAuthentication,
+  restrictTo,
 };
